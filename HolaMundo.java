28333ed950aa6919cd573871f1216b0e1c3d7eb3
Carmen Arroyo Pacheco <Carmen Arroyo Pacheco>
2016-09-26 16:15:45 +0200

    fichero java

---
+++ HolaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Carmen
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola Mundo!");
+    }
+    
+}
+
+ 
+        
+    
